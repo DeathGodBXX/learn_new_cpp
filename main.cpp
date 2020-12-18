16b3bfcf3e823073bb326696bc5c24dabd6b5b9d
DeathGodBXX <2694779216@qq.com>
2020-12-18 23:15:53 +0800

    static,extern using

--- main.cpp
+++ main.cpp
@@ -2,25 +2,11 @@
 #include <thread>
 #include <chrono>
 
-int main()
-{
-    bool bstop = false;
-    std::thread s([&]() {
-        for (int i = 0;; i++)
-        {
-            if (bstop)
-            {
-                std::cout << "结束" << std::endl;
-                break;
-            }
-            std::cout << i << " ";
-        }
-    });
-    std::thread t([&]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        bstop = true;
-        std::cout << "bstop已经修改成true" << std::endl;
-    });
-    s.join();
-    t.join();
-}
+// int main()
+// {
+//     Apple app1, app2;
+//     // app1.i = 1;
+//     // app2.i = 2;
+//     Apple::i = 1;
+//     std::cout << app1.i << std::endl;
+// }
