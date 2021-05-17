28711ce69385d9e88920c5add253c83230351ad9
DeathGodBXX <2694779216@qq.com>
2021-05-17 22:04:12 +0800

    add guptest.cu

---
+++ LeetCode/Test/gputest.cu
@@ -0,0 +1,28 @@
+﻿#include "device_launch_parameters.h"
+#include "cuda_runtime_api.h"
+#include <iostream>
+//需要将C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\VC\\Tools\\MSVC\\14.28.29910\\bin\\Hostx64\\x64
+//添加到环境变量中，其实是使用msvc中的cl.exe可执行文件。但是此时的vscode代码提示不仅包含mingw64的提示，还包括msvc的提示，会显示红色波浪线
+//但是不影响编译结果和运行(估计是二者选一进行调用)。
+//使用nvcc gputest.cu -o gputest.exe 生成可执行文件
+int main()
+{
+    int deviceCount;
+    cudaGetDeviceCount(&deviceCount);
+    for (int i = 0; i < deviceCount; i++)
+    {
+        cudaDeviceProp devProp;
+        cudaGetDeviceProperties(&devProp, i);
+        std::cout << "使用GPU device " << i << ": " << devProp.name << std::endl;
+        std::cout << "设备全局内存总量： " << devProp.totalGlobalMem / 1024 / 1024 << "MB" << std::endl;
+        std::cout << "SM的数量：" << devProp.multiProcessorCount << std::endl;
+        std::cout << "每个线程块的共享内存大小：" << devProp.sharedMemPerBlock / 1024.0 << " KB" << std::endl;
+        std::cout << "每个线程块的最大线程数：" << devProp.maxThreadsPerBlock << std::endl;
+        std::cout << "设备上一个线程块（Block）种可用的32位寄存器数量： " << devProp.regsPerBlock << std::endl;
+        std::cout << "每个EM的最大线程数：" << devProp.maxThreadsPerMultiProcessor << std::endl;
+        std::cout << "每个EM的最大线程束数：" << devProp.maxThreadsPerMultiProcessor / 32 << std::endl;
+        std::cout << "设备上多处理器的数量： " << devProp.multiProcessorCount << std::endl;
+        std::cout << "======================================================" << std::endl;
+    }
+    return 0;
+}
