4fb11d6d4918338a75fdbb15079ba0803188256d
DeathGodBXX <2694779216@qq.com>
2021-05-30 22:23:39 +0800

    add elseif in jumpgame6.cpp

--- LeetCode/Greedy/JumpGame/jumpgame6.cpp
+++ LeetCode/Greedy/JumpGame/jumpgame6.cpp
@@ -88,86 +88,68 @@ using std::vector;
 //         return step[size - 1];
 //     }
 // };
-class Solution
-{
+class Solution {
 public:
-    int minJumps(vector<int> &arr)
-    {
-        if (arr.size() == 1)
-            return 0;
+    int minJumps(vector<int>& arr) { 
+        if(arr.size()==1) return 0;
 
         //arr去重
         remove_dup(arr);
-
-        int size = arr.size();
+        
+        int size=arr.size(); 
         //根据值快速查找对应的索引
-        unordered_multimap<int, int> map;
+        unordered_multimap<int,int> map;
         //deque从中获取可被操作的索引
         queue<int> dp;
-        //定义一个新vector，存储需要的steps
-        vector<int> step(size, INT_MAX);
+       //定义一个新vector，存储需要的steps
+        vector<int> step(size,INT_MAX);
         //初始化mp
-        for (int i = 0; i < size; i++)
-        {
-            map.insert({arr[i], i});
+        for(int i=0;i<size;i++){
+            map.insert({arr[i],i});
         }
         dp.push(0); //index扔到dp中
-        step[0] = 0;
+        step[0]=0;
 
         //核心点在于每次必须到达的最少操作数，都是前面最小操作数+1，对于左侧右侧和同值比较大小
-        int dis = 0;
-        while (!dp.empty())
-        {
+        int dis=0;
+        while(!dp.empty()){
             //每一次穷尽当前dis层下的index
             dis++;
-            int n = dp.size();
-            for (int i = 0; i < n; i++)
-            {
+            int n=dp.size();
+            for(int i=0;i<n;i++){
                 //控制走完当前dis步的索引
-                int front = dp.front();
+                int front=dp.front();
                 dp.pop();
-                if (int before = front - 1; before > 0)
-                    change_step_and_dp(dis, before, dp, step);
-                if (int after = front + 1; after < size)
-                    change_step_and_dp(dis, after, dp, step);
-                auto fds = map.equal_range(arr[front]); //return std::pair<iterator,iterator> start,end
-                for (auto it = fds.first; it != fds.second; it++)
-                {
-                    if (it->second != front)
-                        change_step_and_dp(dis, it->second, dp, step);
+                if(int before=front-1;before>0) change_step_and_dp(dis,before,dp,step);
+                if(int after=front+1;after<size) change_step_and_dp(dis,after,dp,step);
+                else if(after=size-1) return dis-1;
+                auto fds=map.equal_range(arr[front]); //return std::pair<iterator,iterator> start,end
+                for(auto it=fds.first;it!=fds.second;it++){
+                    if(it->second!=front) change_step_and_dp(dis,it->second,dp,step);
                 }
-            }
+            }   
         }
-        return step[size - 1];
+        return step[size-1];
     }
 
     //如果之前记录的值比当前值大的话，修改到达idx的步距和记录dp当前步距的idx
     //这种情况就是设定的INT_MAX,因为后续dis是单调递增的。
-    void change_step_and_dp(int dis, int idx, queue<int> &dp, vector<int> &step)
-    {
-        if (step[idx] > dis)
-        {
-            step[idx] = dis;
+    void change_step_and_dp(int dis,int idx,queue<int>& dp,vector<int>& step){
+        if(step[idx]>dis){
+            step[idx]=dis;
             dp.push(idx);
         }
     }
 
     //arr去重
-    void remove_dup(vector<int> &arr)
-    {
-        bool tag = false;
-        int size = arr.size();
+    void remove_dup(vector<int>& arr){
+        bool tag=false;
+        int size=arr.size();
         vector<int> tmp;
-        for (int i = 0; i < size; i++)
-        {
-            while (i + 1 < size && i + 2 < size && arr[i] == arr[i + 1] && arr[i + 1] == arr[i + 2])
-            {
-                i++;
-                tag = true;
-            }
+        for(int i=0;i<size;i++){
+            while(i+1<size&&i+2<size&&arr[i]==arr[i+1]&&arr[i+1]==arr[i+2]){i++;tag=true;}
             tmp.emplace_back(arr[i]);
         }
-        if (tag)
-            arr = move(tmp); // arr=tmp;
+        if(tag) arr=move(tmp);// arr=tmp;
     }
 };
